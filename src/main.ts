70307a77504174126dd13ce74b05a3805fa26b99
Abhijeet Prasad <aprasad@sentry.io>
2022-02-01 14:38:21 -0500

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as Sentry from '@sentry/capacitor';
+import * as SentryAngular from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
+
 if (environment.production) {
   enableProdMode();
 }
+
+Sentry.init(
+  {
+    integrations: [
+      new Integrations.BrowserTracing({
+        tracingOrigins: ['localhost'],
+      }),
+    ]
+  },
+  // Forward the init method from @sentry/angular
+  SentryAngular.init
+);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
